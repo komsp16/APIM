e7eeea5e0f784170cf99f5b904f003b64f8b121a
komsp16 <kaumil.parekh@outlook.com>
2025-09-30 22:43:47 +1000

    enable swagger ui

--- MovieCatalog.Api/Program.cs
+++ MovieCatalog.Api/Program.cs
@@ -37,11 +37,11 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors();
 app.UseHttpsRedirection();
